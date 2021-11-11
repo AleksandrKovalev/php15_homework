d8900c7e3b6b2445e2aae6b1c2c6cb1c3d4002ad
Aleksandr <kovalev.2014.aleksandr@gmail.com>
2021-11-11 14:09:21 +0200

    hw_10_module

---
+++ lesson_10/first_test.php
@@ -0,0 +1,106 @@
+<?php
+
+    require_once("utils.php");
+
+    /**
+     * Function receives an array with integer numbers,
+     * should return its sum. It is not alowed to use built-in php functions.
+     *
+     * Функция получает на вход массив чисел, должна вернуть сумму этих чисел.
+     * Не использовать встроенные функции суммирования php.
+     *
+     * @param array $arr
+     * @return integer
+     */
+    function my_sum($arr) {
+        $res = 0;
+        foreach ($arr as $value) {
+            $res = $res + $value;
+        }
+        return $res;
+    }
+
+
+    /**
+     * Function receives a long string with many words.
+     * It should return the same string, but words,
+     * larger then 6 symbols, should be changed, symbols
+     * after the sixth one should be replaced by symbol *
+     *
+     * Функция получает на вход длинную строку с множеством слов.
+     * Она должна вернуть ту же строку, но в словах, которые длиннее 6 символов,
+     * функция должна вместо всех символов после шестого поставить одну звездочку.
+     * Пример: Из слова 'verwijdering' должно получиться 'verwij*'
+     *
+     * @param string $shortMe
+     * @return string
+     */
+    function shortener($shortMe) {
+        $arr = explode(' ', $shortMe);
+        foreach($arr as &$value){
+            if (strlen($value) > 6){
+                $value = substr($value, 0, 6) . "*";
+            }
+        $str = implode(' ', $arr);
+        }
+        return $str;
+    }
+
+    /**
+     * Function receives an array of strings.
+     * Please return number of strings, which
+     * length is at least 2 symbols and first character
+     * is equal to the last character
+     *
+     * Функция получает на вход массив строк. Вернуть надо количество строк,
+     * которые не короче двух символов и у которых первый и последний
+     * символ совпадают.
+     *
+     * @param array $arr
+     * @return number
+     */
+    function compare_ends($arr) {
+        foreach($arr as $value){
+            if (strlen($value) >= 2 && $value[0] == $value[strlen($value) - 1]){
+                $a[] = $value;
+                $b = count($a);
+        }    
+      }
+       return $b;
+    }
+
+
+
+    /**
+     * Function receives a string, should return this string reversed.
+     *
+     * Функция получает на вход строку, должна вернуть ее перевернутой.
+     *
+     * @param string $str
+     * @return string
+     */
+    function reverse_string($str) {
+        $arr = preg_split("// ", $str);
+        $rev = array_reverse($arr);
+        $str = implode($rev);
+        
+        return $str;
+    }
+
+test_shortener();
+
+test_compare_ends();
+
+test_my_sum();
+
+test_reverse_string();
+
+
+
+
+
+
+
+
+
+
